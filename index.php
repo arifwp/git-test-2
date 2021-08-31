1ae1e78cc63f630901282ebd7153325397bceee2
arifwp <arifwahyu696@gmail.com>
2021-08-31 10:57:06 +0700

    nambah file index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Ngetes git</title>
+    </head>
+    <body>
+        <h1>hello world!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi corrupti repellat fuga error consequuntur fugit nesciunt. Obcaecati fugiat fugit ipsa porro velit, harum aspernatur dolore, atque beatae, consequatur nam. Tempora.</p>
+    </body>
+</html>
