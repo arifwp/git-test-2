4a1097415cc9bfeb9ff6c31770b6eb0bf0b9fea4
arifwp <arifwahyu696@gmail.com>
2021-08-31 10:57:35 +0700

    nambah ul

--- index.php
+++ index.php
@@ -5,5 +5,10 @@
     <body>
         <h1>hello world!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi corrupti repellat fuga error consequuntur fugit nesciunt. Obcaecati fugiat fugit ipsa porro velit, harum aspernatur dolore, atque beatae, consequatur nam. Tempora.</p>
+        <ul>
+            <li>
+                nama saya
+            </li>
+        </ul>
     </body>
 </html>
